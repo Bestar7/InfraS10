4a580066f0d80740354efa4ed30815b4800c4398
Joachim <joachim.bastin@student.vinci.be>
2022-11-28 11:33:11 +0100

    add DB Key

--- config.js
+++ config.js
@@ -4,6 +4,9 @@ const PgDb = 'exoplanets';
 const PgPassword = 'ipl';
 const PgPort = 5432;
 
+const DATABASE_URL = 'process.env.DATABASE_URL';
+
 module.exports = {
-    ConnectionString: 'postgres://' + PgUser + ':' + PgPassword + '@' + PgHost + ':' + PgPort + '/' + PgDb
+    //ConnectionString: 'postgres://' + PgUser + ':' + PgPassword + '@' + PgHost + ':' + PgPort + '/' + PgDb
+	ConnectionString = DATABASE_URL;
 };
